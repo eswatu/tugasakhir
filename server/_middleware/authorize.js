2a45f442af170596ac7d35ba1867da74217575c4
unknown <iceboltz@gmail.com>
2022-08-15 22:06:55 +0700

    internal update

--- server/_middleware/authorize.js
+++ server/_middleware/authorize.js
@@ -5,20 +5,19 @@ const db = require('../_helpers/db');
 module.exports = authorize;
 
 function authorize(roles = []) {
-    // roles param can be a single role string (e.g. Role.User or 'User') 
-    // or an array of roles (e.g. [Role.Admin, Role.User] or ['Admin', 'User'])
+    // role berupa string atau array
     if (typeof roles === 'string') {
         roles = [roles];
     }
 
     return [
-        // authenticate JWT token and attach decoded token to request as req.user
+        // authenticate JWT token lalu decode ke request req.user
         jwt({ secret, algorithms: ['HS256'] }),
 
-        // attach full user record to request object
+        // implementasikan
         async (req, res, next) => {
             if (roles.length && !roles.includes(req.user.role)){
-                // user's role is not authorized
+                // role tidak sesuai / terotentikasi
                 return res.status(401).json({ message: 'Unauthorized' });
             }
 //auth sukses
